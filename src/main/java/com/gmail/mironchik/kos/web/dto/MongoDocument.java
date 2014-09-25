0d0b2f18332872c715140e7f6237ae6568adb692
koc9n <kos.mironchik@gmail.com>
2014-09-25 20:00:41 +0300

    added mongo library, updated architecture, created new classes for working with mongo DB

---
+++ src/main/java/com/gmail/mironchik/kos/web/dto/MongoDocument.java
@@ -0,0 +1,17 @@
+package com.gmail.mironchik.kos.web.dao;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.math.BigInteger;
+
+/**
+ * Created by k.mironchik on 9/25/2014.
+ */
+@Document
+public class MongoDocument {
+
+    @Id
+    private ObjectId id;
+}
